fdd332eb3fab03328b9f4406cd86976e223984f5
exue <exue1696@gmail.com>
2017-12-01 14:48:57 -0800

    database added

--- public/classes.js
+++ public/classes.js
@@ -1,3 +1,16 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyCi4IbpqA-GvuIs9TJepvosTMbUGQ7Po_E",
+authDomain: "teamsnipelk.firebaseapp.com",
+databaseURL: "https://teamsnipelk.firebaseio.com",
+projectId: "teamsnipelk",
+storageBucket: "teamsnipelk.appspot.com",
+messagingSenderId: "941340425394"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 class Player {
     constructor(name, num, dob, position) {
         this.name = name;
