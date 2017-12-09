60f7e79eedb53a89e514e5dc40b20669bc2129da
Kyle Sit <ktsit@ucsd.edu>
2017-12-09 01:30:58 -0800

    added js file copies

---
+++ public/firebaseModule.js
@@ -0,0 +1,18 @@
+const config = {
+    apiKey: "AIzaSyCi4IbpqA-GvuIs9TJepvosTMbUGQ7Po_E",
+    authDomain: "teamsnipelk.firebaseapp.com",
+    databaseURL: "https://teamsnipelk.firebaseio.com",
+    projectId: "teamsnipelk",
+    storageBucket: "teamsnipelk.appspot.com",
+    messagingSenderId: "941340425394"
+};
+firebase.initializeApp(config);
+var db = firebase.database();
+var storage = firebase.storage();
+var auth = firebase.auth();
+
+export {db, storage, auth};
+    
+
+
+
